6c2d6f07fd2500139b27fc6a82f39089d2813673
JaaayyyyS <130411893+JaaayyyyS@users.noreply.github.com>
2023-10-12 13:04:39 -0300

    SafeAreaView
    Adicionando SafeAreaProvider e insets para ter uma tela segura de sobreposições da StatusBar

--- src/navigation/appnavigation.js
+++ src/navigation/appnavigation.js
@@ -1,2 +1,20 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+
+import HomeScreen from "../screens/home";
+
+const Stack = createNativeStackNavigator()
+
+export default function AppNavigation() {
+    return (
+        <SafeAreaProvider>
+            <NavigationContainer>
+                <Stack.Navigator>
+                    <Stack.Screen name="home" component={HomeScreen} options={{headerShown: false}}/>
+                </Stack.Navigator>
+            </NavigationContainer>
+        </SafeAreaProvider>
+        
+    )
+}
--- src/screens/home.js
+++ src/screens/home.js
@@ -1,14 +1,24 @@
 import { View, Text, SafeAreaView } from "react-native";
 import { StatusBar } from "expo-status-bar";
 
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+
 export default function HomeScreen() {
+    const insets = useSafeAreaInsets()
+
     return(
-        <>
-        <View className="flex-1 justify-center items-center">
+        /* Main Container */
+        <View className="flex-1 justify-center items-center"
+        style={{
+            paddingTop: insets.top,
+            paddingBottom: insets.bottom,
+            paddingRight: insets.right,
+            paddingLeft: insets.left
+        }}
+        >
             <Text className="text-base text-black">
                 Olá mundo!
             </Text>
         </View>
-        </>
     )
 }
